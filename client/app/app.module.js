6a60945d2db2991645015b5d32d0617367b7f237
Gustavo Cañete <gustavoca245@gmail.com>
2015-10-11 19:15:53 -0300

    add angular material, material design styles for welcome page, remove bootstrap

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -8,7 +8,8 @@
         'myApp.addPost',
         'myApp.nav',
         'myApp.firebase',
-        'myApp.auth'
+        'myApp.auth',
+        'ngMaterial'
     ])
 
     .constant('FIREBASE_URL', 'https://blistering-heat-2473.firebaseio.com')
@@ -41,6 +42,32 @@
         function ($urlRouterProvider, authService) {
             $urlRouterProvider.otherwise('/login');
         }
-    ]);
+    ])
+
+    .config(function($mdThemingProvider, $mdIconProvider) {
+
+        $mdIconProvider
+            .defaultIconSet('./assets/svg/avatars.svg', 128)
+            .icon('menu', './assets/svg/menu.svg', 24)
+            .icon('share', './assets/svg/share.svg', 24)
+            .icon('google_plus', './assets/svg/google_plus.svg', 512)
+            .icon('hangouts', './assets/svg/hangouts.svg', 512)
+            .icon('twitter', './assets/svg/twitter.svg', 512)
+            .icon('phone', './assets/svg/phone.svg', 512);
+
+        $mdThemingProvider.theme('default')
+            .dark()
+            .primaryPalette('grey', {
+                'default': '900', // by default use shade 400 from the pink palette for primary intentions
+                'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+                'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+                'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+            })
+            // If you specify less than all of the keys, it will inherit from the
+            // default shades
+            .accentPalette('pink', {
+                'default': '200' // use shade 200 for default, and keep all other shades the same
+            });
+    });
 
 })();
